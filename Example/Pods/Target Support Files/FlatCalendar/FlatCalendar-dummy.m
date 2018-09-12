472a2a27e4212b0a319cc920f0ac9fcdc0c419d5
christianampe <christianampe.work@gmail.com>
2018-09-11 23:05:09 -0700

    moved files around

---
+++ Example/Pods/Target Support Files/FlatCalendar/FlatCalendar-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_FlatCalendar : NSObject
+@end
+@implementation PodsDummy_FlatCalendar
+@end
---
+++ Example/Pods/Target Support Files/Pods-FlatCalendar_Tests/Pods-FlatCalendar_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FlatCalendar_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_FlatCalendar_Tests
+@end
